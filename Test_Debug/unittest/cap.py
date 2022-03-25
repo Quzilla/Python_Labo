1daace4ed474cf6fefc7b9ba4ba2b1954b580e34
Quzilla <63536589+Quzilla@users.noreply.github.com>
2022-03-25 23:48:06 +0900

    edit

---
+++ Test_Debug/unittest/cap.py
@@ -0,0 +1,10 @@
+from os import nice
+
+
+def just_do_it(text: str):
+    "<text>に含まれるすべての単語をタイトルケースに"
+    return text.capitalize()
+
+def just_do_it2(text: str):
+    "<text>に含まれるすべての単語をタイトルケースに"
+    return text.title()
---
+++ Test_Debug/unittest/style1.py
@@ -0,0 +1,14 @@
+"ここにモジュールのdocstringを書く"
+# a = 1
+# b = 2
+# c = 3
+# print(a)
+# print(d)
+
+def func():
+    "ここに関数のdocstringを書く"
+    first = 1
+    second = 2
+    print(first)
+    print(second)
+func()
